3750b394aa17c8b3f2dddf6130a4c7d84c837450
B N LOKESH <lokeshbn2000@gmail.com>
2024-02-29 19:35:19 +0530

    initialcommit

---
+++ script.js
@@ -0,0 +1,22 @@
+let seasonSmallImage = document.getElementById("seasonSmallImage");
+let seasonMediumImage = document.getElementById("seasonMediumImage");
+
+function seasonSpring() {
+    seasonMediumImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-md-img.png";
+    seasonSmallImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-spring-xs-img.png";
+}
+
+function seasonSummer() {
+    seasonMediumImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-md-img.png";
+    seasonSmallImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-summer-xs-img.png";
+}
+
+function seasonAutumn() {
+    seasonMediumImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-md-img.png";
+    seasonSmallImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-autumn-xs-img.png";
+}
+
+function seasonWinter() {
+    seasonMediumImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-md-img.png";
+    seasonSmallImage.src = "https://d1tgh8fmlzexmh.cloudfront.net/ccbp-dynamic-webapps/seasons-switcher-winter-xs-img.png";
+}
